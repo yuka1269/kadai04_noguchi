5180dbdc262fb84f4117c8fb85c12d71307727fe
yuka1269 <yuka.noguchi628@gmail.com>
2024-07-04 12:15:57 +0900

    kadai04

---
+++ design_cafe/JS/script.js
@@ -0,0 +1,33 @@
+
+
+$(function(){
+    $('.slider_g').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      });
+  });
+
+
+  $(function() {
+    const pageTop = $("#js-pageTop").hide();
+  
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+        pageTop.fadeIn();
+      } else {
+        pageTop.fadeOut();
+      }
+    });
+  
+  
+    pageTop.click(function() {
+      $("body,html").animate({
+        scrollTop: 0
+      }, 500);
+      return false;
+    });
+  });
+
+  
+  
